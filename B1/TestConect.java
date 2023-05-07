d29feeffc52bc9a569fc1407b220712dae2d3d7e
dat <datnn1806@gmail.com>
2023-05-07 15:19:13 +0900

    BTVN B1 05/07/23

---
+++ B1/TestConect.java
@@ -0,0 +1,20 @@
+package training;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestConect {
+public static void main(String[] args) {
+	try {
+		Connection con = DatabaseUtil.getConnection();
+		Statement stm = con.createStatement();
+		ResultSet rs = stm.executeQuery("select course_id , title from course");
+		while (rs.next()) {
+			System.out.println(rs.getInt(1)+" - "+rs.getString(2));
+		}
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+}
+}
